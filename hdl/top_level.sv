42d3782eeadf889fd566c7627ebf86610630f353
Christian Grumbein <christian@grumbein.de>
2020-06-09 20:32:11 +0200

    remove unused module

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -66,21 +66,3 @@ module top_level (
 
 endmodule
 
-// BCD (Binary Coded Decimal) counter
-module bcd8_increment (
-	input [7:0] din,
-	output reg [7:0] dout
-);
-	always @* begin
-		case (1'b1)
-			din[7:0] == 8'h 99:
-				dout = 0;
-			din[3:0] == 4'h 9:
-				dout = {din[7:4] + 4'd 1, 4'h 0};
-			default:
-				dout = {din[7:4], din[3:0] + 4'd 1};
-		endcase
-	end
-endmodule
-
-
